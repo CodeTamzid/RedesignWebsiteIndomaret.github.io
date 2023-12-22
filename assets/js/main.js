45a16711b2230592dc911fc0e3b5a8c523ed97e2
CodeAnas <152585647+CodeTamzid@users.noreply.github.com>
2023-12-22 17:11:59 +0700

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,26 +5,75 @@ function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("slide");
-  if (n > slides.length)
-  {
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
     slideIndex = 1;
   }
-  if (n < 1)
-  {
+  if (n < 1) {
     slideIndex = slides.length
   }
-  for (i = 0; i < slides.length; i++)
-  {
+  for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slides[slideIndex-1].style.display = "block";
 
-  
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", " ");
+  }
+  slides[slideIndex -1].style.display = "block";
+  dots[slideIndex -1].className += "active";
 
 }
 
 
 
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1}
+
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 4500); 
+}
+
+
+
+  const slider = new  Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        slidesPerGroup: 1,
+        loop: true,
+        breakpoints: {  
+        '480': {
+        slidesPerView: 2,
+        spaceBetween: 30,},
+        '680': {
+        slidesPerView: 3,
+        spaceBetween: 40,},
+        '780': {
+        slidesPerView: 5.3,
+        spaceBetween: 20, },
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+  
+    })
+
+    
+
+    
